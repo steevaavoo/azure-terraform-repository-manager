f832af2a087ba1311ea3771f12c7a3141f2cfeeb
Steve Baker <steve_m_baker@hotmail.com>
2020-11-28 12:28:56 +0000

    Added build workflow and login script

---
+++ scripts/azure_login.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Error catching
+# -e  Exit immediately if a command exits with a non-zero status.
+# -u  Treat unset variables as an error when substituting. (script will fail if unset variables are found)
+# -o option-name
+#       pipefail     the return value of a pipeline is the status of
+#                    the last command to exit with a non-zero status,
+#                    or zero if no command exited with a non-zero status
+
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+
+# Log in with a service principal using client secret.
+az login --service-principal --username "$ARM_CLIENT_ID" --password "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID" \
+  --subscription "$ARM_SUBSCRIPTION_ID"
