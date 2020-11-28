208876bacb5c48097cbfbd041f018099d9dbadb0
Steve Baker <steve_m_baker@hotmail.com>
2020-11-28 13:23:40 +0000

    Added az tfstate storage

---
+++ scripts/create_tfstate_storage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Resource Group
+echo "STARTED: Creating Resource Group..."
+az group create --location "$LOCATION" --name "$TERRAFORM_STORAGE_RG"
+echo "FINISHED: Creating Resource Group."
+
+# Storage Account
+echo "STARTED: Creating Storage Account..."
+az storage account create --name "$TERRAFORM_STORAGE_ACCOUNT" --resource-group "$TERRAFORM_STORAGE_RG" --location "$LOCATION" --sku Standard_LRS
+echo "FINISHED: Creating Storage Account."
+
+# Storage Container
+echo "STARTED: Creating Storage Container..."
+az storage container create --name "terraform" --account-name "$TERRAFORM_STORAGE_ACCOUNT"
+echo "FINISHED: Creating Storage Container."
