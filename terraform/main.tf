8c8a7d70922207cf090dc8f95b7d9520acbe1381
Steve Baker <steve_m_baker@hotmail.com>
2020-12-14 10:01:39 +0000

    Removed AKS resource to save time in ACR tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,23 +11,23 @@ resource "azurerm_container_registry" "stvacr" {
   admin_enabled            = var.acr_admin_enabled
 }
 
-resource "azurerm_kubernetes_cluster" "stvaks" {
-  name                = var.azurerm_kubernetes_cluster_name
-  resource_group_name = azurerm_resource_group.stvrg.name
-  location            = azurerm_resource_group.stvrg.location
-  dns_prefix          = var.prefix
+# resource "azurerm_kubernetes_cluster" "stvaks" {
+#   name                = var.azurerm_kubernetes_cluster_name
+#   resource_group_name = azurerm_resource_group.stvrg.name
+#   location            = azurerm_resource_group.stvrg.location
+#   dns_prefix          = var.prefix
 
-  default_node_pool {
-    name       = var.node_profile_name
-    node_count = var.node_count
-    vm_size    = var.node_profile_vm_size
-  }
+#   default_node_pool {
+#     name       = var.node_profile_name
+#     node_count = var.node_count
+#     vm_size    = var.node_profile_vm_size
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  tags = {
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Environment = "Dev"
+#   }
+# }
