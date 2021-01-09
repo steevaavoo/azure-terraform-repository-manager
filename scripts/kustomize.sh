2f29c0952ade634616328fb7530bafa5ed0e4ea6
Steve Baker <steve_m_baker@hotmail.com>
2021-01-09 14:04:38 +0000

    Changed tagging

--- scripts/kustomize.sh
+++ scripts/kustomize.sh
@@ -6,6 +6,6 @@ trap "echo 'error: Script failed: see failed command above'" ERR
 cd ../manifests
 
 # Update kustomization.yaml file
-kustomize edit set image "$CONTAINER_IMAGE_TAG_FULL"
+kustomize edit set image nodeapp="$CONTAINER_IMAGE_FQDN:$CONTAINER_IMAGE_TAG"
 
 # Customisations are applied in deploy-manifests.ps1 with --kustomize argument
