879d9368b9b640eb926a9ed631ed06845ddd5c45
Steve Baker <steve_m_baker@hotmail.com>
2021-02-06 12:49:12 +0000

    Added helm terraform chart

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,8 +5,17 @@ terraform {
   backend "azurerm" {
     # storage_account_name = ""
     # container_name       = "terraform"
-    key                  = "terraform.tfstate"
+    key = "terraform.tfstate"
     # access_key           = ""
+  }
+
+  # providers (pin all versions)
+  # versioning syntax: https://www.terraform.io/docs/configuration/modules.html#module-versions
+  required_providers {
+    # https://github.com/hashicorp/terraform-provider-helm/releases
+    helm = "2.0.2"
+    # https://github.com/hashicorp/terraform-provider-kubernetes/releases
+    kubernetes = "2.0.2"
   }
 }
 
@@ -14,4 +23,22 @@ terraform {
 provider "azurerm" {
   version = "2.44.0"
   features {}
+}
+
+# Helm/K8s Connectors
+# use statically defined credentials
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs#statically-defined-credentials
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].cluster_ca_certificate)
+}
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.stvaks.kube_admin_config[0].cluster_ca_certificate)
+  }
 }
